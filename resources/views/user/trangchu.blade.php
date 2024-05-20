3bd702e623eb8159a4991187e9bd433992b0218f
Nope <nothingmore2003@gmail.com>
2024-05-20 11:00:16 +0700

    a

---
+++ resources/views/user/trangchu.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <section>
+        <header>
+            <div class="filter">
+                <nav class="container">
+                    <a href="" id="logo">
+                        <img src="images/logo.png" alt="">
+                    </a>
+                    <ul id="main-menu">
+                        <li><a href="">Test 1</a></li>
+                        <li><a href="">Test 1</a></li>
+                        <li><a href="">Test 1</a></li>
+                        <li><a href="">Test 1</a>
+                            <ul class="sub-menu">
+                                <li><a href="">Test 2</a></li>
+                                <li><a href="">Test 2</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="">Test 3</a></li>
+                                        <li><a href="">Test 3</a></li>
+                                        <li><a href="">Test 3</a></li>
+                                        <li><a href="">Test 3</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="">Test 2</a></li>
+                                <li><a href="">Test 2</a></li>
+                            </ul>
+                        </li>   
+                        <li><a href="">Test 1</a></li>
+                        <li><a href="">Test 1</a></li>
+                        <li><a href="">Test 1</a></li>
+                        <li><a href="">Test 1</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    </section>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
